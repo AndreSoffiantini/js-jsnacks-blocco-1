9598041ae196ed757bf6ef3973408fba9a54049d
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-03-30 11:51:05 +0200

    Add check in third snack and create folders for each snack

--- assets/js/snack-3/main-3.js
+++ assets/js/snack-3/main-3.js
@@ -3,13 +3,22 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
 let sum = 0;
+let counter = 0;
 
-for (let i = 0; i < 10; i++) {
+for (let i = 1; i <= 10; i++) {
 
     let number = parseInt(prompt("Inserisci un numero"));
 
+    if (isNaN(number)) {
+
+        alert("Numero non inserito!");
+        continue;
+    }
+
+    counter++;
     sum += number;
 
 }
 
+console.log(`I numeri inseriti sono ${counter}`);
 console.log(`La somma dei numeri inseriti è ${sum}`);
